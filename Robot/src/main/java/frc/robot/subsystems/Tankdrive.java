ec3ff18e11b7c05116b082e285204d8780fe9e84
Nick Valerio <51347399+n-valerio@users.noreply.github.com>
2022-01-15 11:00:32 -0500

    Move tankdrive to subsystem

---
+++ Robot/src/main/java/frc/robot/subsystems/Tankdrive.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Tankdrive extends SubsystemBase {
+    VictorSPX _victor1 = new VictorSPX(1);
+    VictorSPX _victor3 = new VictorSPX(3);
+    Joystick _joystick = new Joystick(0);
+    public Tankdrive(){
+    }
+
+    @Override
+    public void periodic() {
+     // This method will be called once per scheduler run
+        double stick1 = _joystick.getRawAxis(1);// left y?
+        double stick0 = _joystick.getRawAxis(2);// left x? 3 right x+y
+
+        _victor1.set(ControlMode.PercentOutput, stick0/-1.5);//offset should reduce speed
+        _victor3.set(ControlMode.PercentOutput, stick1/1.5);
+    }
+
+    @Override
+    public void simulationPeriodic() {
+     // This method will be called once per scheduler run during simulation
+    }
+    
+}
