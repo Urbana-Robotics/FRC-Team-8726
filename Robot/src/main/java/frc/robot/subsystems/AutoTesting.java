52bb75742e1b0c8eb70754d1c330990b4789649d
emilyy-wang <97705309+Urbana-Robotics@users.noreply.github.com>
2022-01-21 16:24:06 -0500

    Add time based auto testing

--- Robot/src/main/java/frc/robot/subsystems/AutoTesting.java
+++ Robot/src/main/java/frc/robot/subsystems/AutoTesting.java
@@ -4,33 +4,46 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 //import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
 
 
 // WPILib Gyro Documentation: https://docs.wpilib.org/en/stable/docs/software/hardware-apis/sensors/gyros-software.html?highlight=Gyro#the-gyro-interface
 // Basic NavX Gyro Concepts: https://pdocs.kauailabs.com/navx-mxp/installation/orientation-2/
 public class AutoTesting extends SubsystemBase{
+  Timer timer;
+  public AutoTesting() {
+    timer = new Timer();
+     timer.start();   
+    
+
+  }
   
-  public AutoTesting() {}
-  
+
   VictorSPX victorRight = new VictorSPX(3);
   VictorSPX victorLeft = new VictorSPX(1);
   //AHRS gyro = new AHRS(SPI.Port.kMXP); //may be moved into sensors class
   boolean isDriving = true;
   int timeDriven = 0;
+
   
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    boolean check = timer.hasElapsed(5);
+    if (check){
+      isDriving = false;
+      }
+    else{
+        isDriving = true;
+      }
+
     if (isDriving){
     victorRight.set(ControlMode.PercentOutput, -0.45);
     victorLeft.set(ControlMode.PercentOutput, 0.45);
     }
-    timeDriven++;
-    if (timeDriven > 3){ //may be too small based on scheduler frequency
-      isDriving = false;
-    }
-   
+    
   }
+
 
   @Override
   public void simulationPeriodic() {
