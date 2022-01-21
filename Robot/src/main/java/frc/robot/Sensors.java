556f88d71075f3729a52605efc04c6563f778981
Nick Valerio <n-valerio@users.noreply.github.com>
2022-01-21 17:12:36 -0500

    Add additional NavX docs references

--- Robot/src/main/java/frc/robot/Sensors.java
+++ Robot/src/main/java/frc/robot/Sensors.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.SPI;
 
 // WPILib Gyro Documentation: https://docs.wpilib.org/en/stable/docs/software/hardware-apis/sensors/gyros-software.html?highlight=Gyro#the-gyro-interface
 // Basic NavX Gyro Concepts: https://pdocs.kauailabs.com/navx-mxp/installation/orientation-2/
+// NavX Docs: https://www.kauailabs.com/public_files/navx-mxp/apidocs/java/com/kauailabs/navx/frc/package-summary.html
 public class Sensors {
     public Sensors(){}
     private AHRS gyro = new AHRS(SPI.Port.kMXP);
