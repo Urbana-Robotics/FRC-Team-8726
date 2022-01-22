9347f2196352d2ca0dd5ebe9416edc16d5d8b1b1
Nick Valerio <n-valerio@users.noreply.github.com>
2022-01-22 09:37:19 -0500

    Move Sensors to subsystem

--- Robot/src/main/java/frc/robot/subsystems/Sensors.java
+++ Robot/src/main/java/frc/robot/subsystems/Sensors.java
@@ -1,12 +1,13 @@
-package frc.robot;
+package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // WPILib Gyro Documentation: https://docs.wpilib.org/en/stable/docs/software/hardware-apis/sensors/gyros-software.html?highlight=Gyro#the-gyro-interface
 // Basic NavX Gyro Concepts: https://pdocs.kauailabs.com/navx-mxp/installation/orientation-2/
 // NavX Docs: https://www.kauailabs.com/public_files/navx-mxp/apidocs/java/com/kauailabs/navx/frc/package-summary.html
-public class Sensors {
+public class Sensors extends SubsystemBase {
     public Sensors(){}
     private AHRS gyro = new AHRS(SPI.Port.kMXP);
 
@@ -14,6 +15,15 @@ public class Sensors {
         return gyro;
     }
     
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+  
+    @Override
+    public void simulationPeriodic() {
+      // This method will be called once per scheduler run during simulation
+    }
 }
 
 
