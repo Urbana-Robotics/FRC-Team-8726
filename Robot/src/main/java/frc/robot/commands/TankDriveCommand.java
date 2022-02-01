a387d3f8c37882b6cc4b93a722d1df27da263344
Nick Valerio <n-valerio@users.noreply.github.com>
2022-02-01 15:28:42 -0500

    remove navx import

--- Robot/src/main/java/frc/robot/commands/TankDriveCommand.java
+++ Robot/src/main/java/frc/robot/commands/TankDriveCommand.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
